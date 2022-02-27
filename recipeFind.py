5a6bd574ecc642f5fbc39ce983580560f1ec456a
Jarred-Zhang <95109943+Jarred-Zhang@users.noreply.github.com>
2022-02-26 19:18:08 -0800

    Update recipeFind.py

--- recipeFind.py
+++ recipeFind.py
@@ -1,27 +1,27 @@
 # recipe finder 
 
-def tformat(x):
-    if isinstance(x, int):
-        x=str(x)
-    if int(x)<0:
-        return "invalid time"
-    elif x=="0":
+# Format a time in a human readable format.
+def tformat(mins):
+    mins=int(mins)
+    times = [("hr", 60),("min", 1)]
+    if not mins:
         return ""
-    elif x=="1":
-        return "1 min"
-    elif 1<int(x)<60:
-        return x+" mins"
-    elif x=="60":
-        return "1 hr"
-    elif 120>int(x)>60:
-        return "1 hr " +str(int(x)%60)+" mins"
-    elif int(each[2])>120 and int(each[2])%60!=0:
-        return str(int(each[2])//60)+" hrs " +str(int(each[2])%60)+" mins"
-    elif int(each[2])>60 and int(each[2])%60==0:
-        return str(int(each[2])//60)+" hrs "
+    time = []
+    for i, j in times:
+        qty = mins // j
+        if qty:
+            if qty > 1:
+                i += "s"
+            time.append(str(qty) + " " + i)
+        mins %= j
+    return ' '.join(time)
 
+
+
+# Open a recipeLinks. txt file
 lines = [i.rstrip("\n") for i in open('recipeLinks.txt', encoding="utf8", errors="replace")]
 
+# Open a history. txt file.
 history = open("history.txt","a")
 
 #ingredients included by default, optional for neglection
